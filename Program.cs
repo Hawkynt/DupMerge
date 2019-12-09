e90faca53a4988cc72d368c483caa6b9b0b77f24
Hawkynt <github@synthelicz.de>
2019-12-09 19:33:46 +0100

    + LFN support

--- Program.cs
+++ Program.cs
@@ -32,6 +32,7 @@ namespace DupMerge {
       public bool RemoveSymbolicLinks { get; set; }
       public bool DeleteSymbolicLinkedFiles { get; set; }
       public int MaximumCrawlerThreads { get; set; } = Math.Min(Environment.ProcessorCount, 8);
+      public bool ShowInfoOnly { get; set; }
     }
 
     private class FileEntry {
