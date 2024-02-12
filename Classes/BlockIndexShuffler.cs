7afb1bd0b0becbc265c9b64ae1508aa313e73dd6
Hawkynt <github@synthelicz.de>
2024-02-12 17:19:36 +0100

    + namespaces

--- Classes/BlockIndexShuffler.cs
+++ Classes/BlockIndexShuffler.cs
@@ -1,6 +1,8 @@
 #nullable enable
 using System.Collections.Generic;
 
+namespace Classes;
+
 internal static class BlockIndexShuffler {
   /// <summary>
   /// Creates a stream of block indexes, which are not simply following each other the get better chances to detect differences early.
@@ -9,7 +11,7 @@ internal static class BlockIndexShuffler {
   /// <param name="blockCount">The block count.</param>
   /// <returns>Block indices</returns>
   public static IEnumerable<long> Shuffle(long blockCount) {
-    var lowerBlockIndex = 0;
+    var lowerBlockIndex = 0L;
     var upperBlockIndex = blockCount - 1;
 
     while (lowerBlockIndex < upperBlockIndex) {
